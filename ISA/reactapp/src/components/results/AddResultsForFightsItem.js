22053e0513ecbffb826521b465bf38ea2c09c160
SimeunovicB <30902647+SimeunovicB@users.noreply.github.com>
2021-08-07 15:20:13 +0200

    Listing for adding results

--- ISA/reactapp/src/components/results/AddResultsForFightsItem.js
+++ ISA/reactapp/src/components/results/AddResultsForFightsItem.js
@@ -1,9 +1,10 @@
-import classes from "./FightItem.module.css";
+import classes from "./AddResultsForFightsItem.module.css";
 import Card from "../ui/Card";
 import axios from "axios";
 import { useState, useEffect } from "react";
+import Select from "react-select";
 
-function FightItem(props) {
+function AddResultsForFightsItem(props) {
   const [redCornerFighterName, setRedCornerFighterName] = useState(null);
   const [blueCornerFighterName, setBlueCornerFighterName] = useState(null);
 
@@ -13,11 +14,15 @@ function FightItem(props) {
   const [redCornerFighterImg, setRedCornerFighterImg] = useState(null);
   const [blueCornerFighterImg, setBlueCornerFighterImg] = useState(null);
 
+  const [selectedOptionWinner, setSelectedOptionWinner] = useState(null);
+  const [selectedOptionMethod, setSelectedOptionMethod] = useState("");
+
+  const [loadedFighters, setLoadedFighters] = useState([]);
+  const [methodOptions, setMethodOptions] = useState([{value: 1, label:"Decision"},{value: 2, label:"KO/TKO"},{value: 3, label: "Submission"}])
 
   console.log("RED CORNER FIGHTER ", props.redCornerFighter);
   console.log("BLUE CORNER FIGHTER ", props.blueCornerFighter);
   console.log("WINNER_ID ", props.winner_id);
-
 
   useEffect(() => {
     axios({
@@ -29,6 +34,7 @@ function FightItem(props) {
       setBlueCornerFighterName(blueFighter.name);
       setBlueCornerFighterSurname(blueFighter.surname);
       setBlueCornerFighterImg(blueFighter.image);
+      loadedFighters.push({value: blueFighter.id, label: blueFighter.name});
     });
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
@@ -42,14 +48,20 @@ function FightItem(props) {
       setRedCornerFighterName(redFighter.name);
       setRedCornerFighterSurname(redFighter.surname);
       setRedCornerFighterImg(redFighter.image);
+      loadedFighters.push({value: redFighter.id, label: redFighter.name});
     });
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  const submitHandler = () => {
+    console.log("submitHandler");
+  };
 
   return (
     <li className={classes.item}>
       <Card>
         <div className={classes.content}>
-          {redCornerFighterName} {redCornerFighterSurname} vs {blueCornerFighterName} {blueCornerFighterSurname}
+          {redCornerFighterName} {redCornerFighterSurname} vs{" "}
+          {blueCornerFighterName} {blueCornerFighterSurname}
         </div>
         <div className={classes.content}>
           <img
@@ -58,7 +70,7 @@ function FightItem(props) {
             height="200"
             width="200"
           ></img>{" "}
-          vs
+          vs{" "}
           <img
             src={blueCornerFighterImg}
             alt="slika borca"
@@ -66,9 +78,31 @@ function FightItem(props) {
             width="200"
           ></img>
         </div>
+
+        <form className={classes.form} onSubmit={submitHandler}>
+          <div className={classes.control}>
+            <label htmlFor="redCornerFighter">Winner</label>
+            <Select
+              defaultValue={selectedOptionWinner}
+              onChange={setSelectedOptionWinner}
+              options={loadedFighters}
+            />
+          </div>
+          <div className={classes.control}>
+            <label htmlFor="blueCornerFighter">Method</label>
+            <Select
+              defaultValue={selectedOptionMethod}
+              onChange={setSelectedOptionMethod}
+              options={methodOptions}
+            />
+          </div>
+          <div className={classes.actions}>
+            <button>Add result</button>
+          </div>
+        </form>
       </Card>
     </li>
   );
 }
 
-export default FightItem;
+export default AddResultsForFightsItem;
--- ISA/reactapp/src/components/results/AddResultsForFightsList.js
+++ ISA/reactapp/src/components/results/AddResultsForFightsList.js
@@ -1,6 +1,9 @@
 // import FightItem from "./FightItem";
+import AddResultsForFightsItem from "./AddResultsForFightsItem";
 import classes from "./AddResultsForFightsList.module.css";
 import { useLocation } from "react-router-dom";
+import { useState, useEffect } from 'react';
+import axios from "axios";
 
 function AddResultsForFightsList() {
 
@@ -8,22 +11,37 @@ function AddResultsForFightsList() {
     console.log(location);
     console.log(location.state.eventId);
 
+    const [fights, setFights] = useState([]);
+
+    useEffect(() => {
+      axios({
+        method: 'get',
+        url: 'api/fights/event?eventId=' + location.state.eventId
+      }).then(response => {
+        console.log(response);
+        console.log(response.data);
+        const fights = response.data;
+        setFights(fights);
+      }).catch(error => {
+        console.log(error);
+      })
+    }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
 //   console.log("FAJTERI U FIGHTER LIST", props.fights);
 
   return (
-      <div>Ide gas</div>
-    // <ul className={classes.list}>
-    //   {props.fights.map((fight) => (
-    //     <FightItem
-    //       key={fight.id}
-    //       id={fight.id}
-    //       redCornerFighter={fight.redCornerFighter}
-    //       blueCornerFighter={fight.blueCornerFighter}
-    //       method={fight.method}
-    //       date={fight.date}
-    //     />
-    //   ))}
-    // </ul>
+    <ul className={classes.list}>
+      {fights.map((fight) => (
+        <AddResultsForFightsItem
+          key={fight.id}
+          id={fight.id}
+          redCornerFighter={fight.redCornerFighter}
+          blueCornerFighter={fight.blueCornerFighter}
+          method={fight.method}
+          date={fight.date}
+        />
+      ))}
+    </ul>
   );
 }
 export default AddResultsForFightsList;
